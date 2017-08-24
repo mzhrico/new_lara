5382a14bbb3ab58646220666624374597611ba86
RICO MAI <ricomai@ricos-mac.home>
2017-08-23 20:03:42 -0400

    second change

--- app/routes.php
+++ app/routes.php
@@ -16,7 +16,4 @@ Route::get('/', function()
 	return View::make('hello');
 });
 
-Route::get('/wawa',array(
-    'as'=>"get",
-    'uses'=>'PostController@index'
-));
+Route::resource('post','PostController');
